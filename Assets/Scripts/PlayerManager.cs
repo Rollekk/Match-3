3b107f6454c21681fe6ced75a2911f062d835c51
Rollekk <rollek97@gmail.com>
2021-08-29 20:32:26 +0200

    Added restart button

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -31,4 +32,8 @@ public class PlayerManager : MonoBehaviour
         Application.Quit();
     }
 
+    public void ResetButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
